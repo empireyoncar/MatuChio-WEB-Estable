d4ba2266ee3df9f7125eb45a35f18a8916da781c
empireyoncar <116581743+empireyoncar@users.noreply.github.com>
2026-03-02 17:28:11 +0000

    prueba de actualizacion de pagina

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 export default function Home() {
   return (
-    <div style={{ width: "350px", height: "250px", overflow: "hidden" }}>
-      <iframe
-        src="/proyectos/slider3d/dist/index.html"
-        width="100%"
-        height="100%"
-        style={{ border: "none" }}
-      />
+    <div>
+      {/* Página sin noticias */}
     </div>
   );
 }
