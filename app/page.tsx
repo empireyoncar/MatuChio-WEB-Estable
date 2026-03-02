258c11dc45a9634dfbc819da4a6b9e2d0a2c7194
empireyoncar <116581743+empireyoncar@users.noreply.github.com>
2026-03-02 18:38:07 +0000

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <div>
-      {/* Página sin noticias */}
+      <div style={{ width: "350px", height: "250px", overflow: "hidden" }}>
+  <iframe
+    src="/proyectos/slider3d/dist/index.html"
+    width="100%"
+    height="100%"
+    style={{ border: "none" }}
+  />
+  </div>
+
     </div>
   );
 }
