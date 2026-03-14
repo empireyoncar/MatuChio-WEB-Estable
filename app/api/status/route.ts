986487067bb8b831f1eef01ad249b00957652d0f
empireyoncar <116581743+empireyoncar@users.noreply.github.com>
2026-03-14 15:16:38 +0000

    quite log de consola

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 
-// 🔥 Fuerza a Next.js a NO cachear este endpoint
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
@@ -8,14 +7,11 @@ export async function GET() {
   try {
     const GAME_URL = process.env.GAMESERVER_URL || "http://192.168.1.178:8081";
 
-    console.log("URL USADA:", GAME_URL);
-
     const res = await fetch(`${GAME_URL}/api/status`, {
-      cache: "no-store" // evita cache del fetch
+      cache: "no-store"
     });
 
     if (!res.ok) {
-      console.log("FETCH ERROR:", res.status);
       return NextResponse.json(
         { error: "GameServer unreachable" },
         { status: 500 }
@@ -26,13 +22,10 @@ export async function GET() {
 
     try {
       raw = await res.json();
-      console.log("RAW DATA:", raw);
-    } catch (err) {
-      console.log("JSON PARSE ERROR:", err);
+    } catch {
       raw = {};
     }
 
-    // Detectamos automáticamente la lista de jugadores
     let playersList: any[] = [];
 
     if (Array.isArray(raw.playersList)) {
@@ -54,8 +47,7 @@ export async function GET() {
       { status: 200 }
     );
 
-  } catch (err) {
-    console.log("GENERAL ERROR:", err);
+  } catch {
     return NextResponse.json(
       { error: "Internal error" },
       { status: 500 }
